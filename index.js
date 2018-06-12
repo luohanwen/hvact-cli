42b12be476b21b73613df97784200637182b01f8
haven <641136947@qq.com>
2018-06-12 15:02:39 +0800

    test

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const symbols = require("log-symbols");
 
 program
     .version("1.0.0", "-v,--version")
-    .command("init <name>", "generate a new project from a template")
+    .command("init <name>")
     .action(name => {
         if (!fs.existsSync(name)) {
             inquirer
