86871f9cfa6d1568d78ec911cfe07bb371d36475
haven <641136947@qq.com>
2018-06-12 09:40:00 +0800

    初步搭建

---
+++ index.js
@@ -0,0 +1,38 @@
+#!/user/bin/env node
+const fs = require("fs");
+const program = require("commander");
+const download = require("download-git-repo");
+const handlebars = require("handlebars");
+const inquirer = require("inquirer");
+const ora = require("ora");
+const chalk = require("chalk");
+const symbols = require("log-symbols");
+
+program
+    .version("1.0.0", "-v,--version")
+    .command("init <name>")
+    .action(name => {
+        if (!fs.existsSync(name)) {
+            inquirer
+                .prompt([{ name: "author", message: "请输入作者名称:" }])
+                .then(answers => {
+                    const spinner = ora("正在下载模板…");
+                    spinner.start();
+                    download("luohanwen/vue-practice", name, err => {
+                        if (err) {
+                            spinner.fail();
+                            console.log(symbols.error, chalk.red(err));
+                        } else {
+                            spinner.succeed();
+                            console.log(
+                                symbols.success,
+                                chalk.green("项目初始化完成")
+                            );
+                        }
+                    });
+                });
+        } else {
+            console.log(symbols.error, chalk.red("项目已存在"));
+        }
+    });
+program.parse(process.argv);
